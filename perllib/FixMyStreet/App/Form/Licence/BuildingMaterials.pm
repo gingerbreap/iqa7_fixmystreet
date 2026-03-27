mysociety/fixmystreet

[TfL] Add highways licence application forms.
Add foundation for TfL's digital licence application system, replacing
their current process.

- Licence controller that discovers and routes to specific licence form
  types (scaffold, etc.)
- Base Licence form role providing shared functionality for all licence
  forms (file uploads, summary display, field formatting)
- Form submission creates non-public Problem records with licence data
  stored in extra metadata

Co-authored-by: Chris Mytton <chrism@mysociety.org>
Co-authored-by: Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
Co-authored-by: Struan Donald <struan@mysociety.org>
Co-authored-by: Moray Jones <moray@mysociety.org>
Co-authored-by: Nik Gupta <vngupta77@gmail.com>
Co-authored-by: Dave Arter <davea@mysociety.org>
Co-authored-by: jacquelinebylau

perllib/FixMyStreet/App/Form/Licence/BuildingMaterials.pm
@@ -1,0 +1,237 @@
+package FixMyStreet::App::Form::Licence::BuildingMaterials;
+
+use HTML::FormHandler::Moose;
+extends 'FixMyStreet::App::Form::Licence::Base';
+use utf8;
+
+# Type identifier used in URL
+sub type { 'building-materials' }
+
+# Human-readable name for display
+sub name { 'Building materials' }
+
+sub tandc_link { 'https://content.tfl.gov.uk/building-materials-guidance-notes-and-terms-conditions.pdf' }
+
+sub next_after_contractor { 'activity' }
+
+# ==========================================================================
+# Introduction / Before you get started
+# ==========================================================================
+has_page intro => (
+    fields => ['start'],
+    title => 'Building materials Licence Application',
+    intro => 'building-materials/intro.html',
+    next => 'location',
+);
+
+has_field start => (
+    type => 'Submit',
+    value => 'Start application',
+    element_attr => { class => 'govuk-button' },
+);
+
+# ==========================================================================
+# Location (fields from Fields::Location role)
+# ==========================================================================
+has_page location => (
+    fields => ['building_name_number', 'street_name', 'borough', 'postcode', 'continue'],
+    title => 'Location of building materials',
+    intro => 'location.html',
+    next => 'dates',
+    post_process => sub {
+        my $form = shift;
+        $form->post_process_location;
+    },
+);
+
+# ==========================================================================
+# Activity
+# ==========================================================================
+has_page activity => (
+    fields => ['activity', 'building_materials_stored', 'continue'],
+    title => 'Purpose of the building materials',
+    next => 'site_pedestrian_space',
+);
+
+has_field activity => (
+    type => 'Text',
+    widget => 'Textarea',
+    label => 'What activity will the materials be used for?',
+    required => 1,
+    tags => {
+        hint => 'For example ‘new build’, ‘demolition’ or ‘building repair’',
+    },
+);
+
+has_field building_materials_stored => (
+    type => 'Text',
+    widget => 'Textarea',
+    label => 'What type of materials will be stored?',
+    required => 1,
+    tags => {
+        hint => 'For example ‘bagged sand’, ‘bricks’ or ‘bagged cement’',
+    },
+);
+
+# ==========================================================================
+# Site Specific Information (licence-specific questions)
+# Split into one question per page sometimes for better UX with long labels
+# ==========================================================================
+has_page site_pedestrian_space => (
+    fields => ['footway_incursion', 'site_adequate_space', 'continue'],
+    title => 'Pedestrian space',
+    next => 'site_carriageway_distance',
+);
+
+has_field footway_incursion => (
+    type => 'Text',
+    label => 'What is the proposed footway incursion?',
+    required => 1,
+    tags => {
+        hint => 'For example, ‘1m from building line and 3m unobstructed footway’ or ‘no footway incursion’',
+    },
+);
+
+has_field site_adequate_space => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    label => 'Will pedestrian space be maintained in line with TfL requirements - 2m for lightly used footways, 3m for medium-use footways and 4m for busy footways, with no reduction of width for intensely used footways?',
+    required => 1,
+    tags => { hint => 'If no, then a site meeting between the applicant and TfL may be required.' },
+    options => [
+        { label => 'Yes', value => 'Yes' },
+        { label => 'No', value => 'No' },
+    ],
+);
+
+# ==========================================================================
+has_page site_carriageway_distance => (
+    fields => ['carriageway_incursion', 'site_within_450mm', 'continue'],
+    title => 'Carriageway impact',
+    next => 'site_infrastructure',
+);
+
+has_field carriageway_incursion => (
+    type => 'Text',
+    label => 'What is the proposed carriageway incursion?',
+    required => 1,
+    tags => {
+        hint => 'For example, ‘no carriageway incursion’ or ‘Materials located in loading bay’',
+    },
+);
+
+has_field site_within_450mm => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    label => 'Will any materials be stored within 450mm of the carriageway edge?',
+    tags => { hint => 'If yes, then a site meeting between the applicant and TfL may be required.' },
+    required => 1,
+    options => [
+        { label => 'Yes', value => 'Yes' },
+        { label => 'No', value => 'No' },
+    ],
+);
+
+# ==========================================================================
+has_page site_infrastructure => (
+    fields => ['site_obstruct_infrastructure', 'continue'],
+    title => 'Street infrastructure',
+    next => 'have_you_considered',
+);
+
+has_field site_obstruct_infrastructure => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    label => 'Will the materials obstruct or obscure any of the following: traffic signal, traffic signal controller, bus stop, pedestrian crossing, junction sight line, road lighting column, traffic sign, parking bay, or any ‘ironwork’ in the highway or other street furniture?',
+    tags => { hint => 'If yes, a site meeting between the applicant and TfL may be required.' },
+    required => 1,
+    options => [
+        { label => 'Yes', value => 'Yes' },
+        { label => 'No', value => 'No' },
+    ],
+);
+
+# ==========================================================================
+# Have you considered? (TCSR/TTRO + T&Cs)
+# Fields from Fields::TemporaryProhibition role
+# ==========================================================================
+has_page have_you_considered => (
+    fields => [
+        'parking_dispensation',
+        'parking_bay_suspension',
+        'bus_stop_suspension',
+        'bus_lane_suspension',
+        'road_closure_required',
+        'tcsr_website_note',
+        'continue'
+    ],
+    title => 'Additional considerations',
+    intro => 'building-materials/have_you_considered.html',
+    next => 'terms',
+);
+
+has_page terms => (
+    fields => [
+        'terms_accepted',
+        'continue'
+    ],
+    title => 'Terms and conditions confirmation',
+    next => 'uploads',
+);
+
+# ==========================================================================
+# Upload required documents
+# ==========================================================================
+my $upload_fields = [
+    'upload_insurance',
+    'insurance_validity',
+    'upload_rams',
+    'upload_site_drawing',
+    'upload_additional',
+    'continue'
+];
+has_page uploads => (
+    fields => $upload_fields,
+    title => 'Upload required documents',
+    intro => 'uploads.html',
+    next => 'payment',
+    update_field_list => sub {
+        my ($form) = @_;
+        my $fields = {};
+        foreach (@$upload_fields) {
+            next unless $_ =~ /^upload_/;
+            $form->handle_upload($_, $fields);
+        }
+        return $fields;
+    },
+    post_process => sub {
+        my ($form) = @_;
+        foreach (@$upload_fields) {
+            next unless $_ =~ /^upload_/;
+            $form->process_upload($_);
+        }
+    },
+);
+
+has_field upload_site_drawing => (
+    type => 'FileIdUpload',
+    label => 'Site drawing',
+    tags => {
+        hint => 'Including measurements and available space maintained for pedestrians',
+    },
+    messages => {
+        upload_file_not_found => 'Please upload a site drawing',
+    },
+);
+
+has_field upload_additional => (
+    type => 'FileIdUpload',
+    label => 'Additional supporting documentation',
+    messages => {
+        upload_file_not_found => 'Please upload any additional supporting documentation',
+    },
+);
+
+__PACKAGE__->meta->make_immutable;
+
+1;

gingerbreap/iqa7_fixmystreet

perllib/FixMyStreet/App/Form/Licence/BuildingMaterials.pm
@@ -1,0 +1,236 @@
+package FixMyStreet::App::Form::Licence::BuildingMaterials;
+
+use HTML::FormHandler::Moose;
+extends 'FixMyStreet::App::Form::Licence::Base';
+use utf8;
+
+# Type identifier used in URL
+sub type { 'building-materials' }
+
+# Human-readable name for display
+sub name { 'Building materials' }
+
+sub tandc_link { 'https://content.tfl.gov.uk/building-materials-guidance-notes-and-terms-conditions.pdf' }
+
+sub next_after_contractor { 'activity' }
+
+# ==========================================================================
+# Introduction / Before you get started
+# ==========================================================================
+has_page intro => (
+    fields => ['start'],
+    title => 'Building materials licence application',
+    intro => 'building-materials/intro.html',
+    next => 'location',
+);
+
+has_field start => (
+    type => 'Submit',
+    value => 'Start application',
+    element_attr => { class => 'govuk-button' },
+);
+
+# ==========================================================================
+# Location (fields from Fields::Location role)
+# ==========================================================================
+has_page location => (
+    fields => ['building_name_number', 'street_name', 'borough', 'postcode', 'continue'],
+    title => 'Location of building materials',
+    intro => 'location.html',
+    next => 'dates',
+    post_process => sub {
+        my $form = shift;
+        $form->post_process_location;
+    },
+);
+
+# ==========================================================================
+# Activity
+# ==========================================================================
+has_page activity => (
+    fields => ['activity', 'building_materials_stored', 'continue'],
+    title => 'Purpose of the building materials',
+    next => 'site_pedestrian_space',
+);
+
+has_field activity => (
+    type => 'Text',
+    widget => 'Textarea',
+    label => 'What activity will the materials be used for?',
+    required => 1,
+    tags => {
+        hint => 'For example ‘new build’, ‘demolition’ or ‘building repair’',
+    },
+);
+
+has_field building_materials_stored => (
+    type => 'Text',
+    widget => 'Textarea',
+    label => 'What type of materials will be stored?',
+    required => 1,
+    tags => {
+        hint => 'For example ‘bagged sand’, ‘bricks’ or ‘bagged cement’',
+    },
+);
+
+# ==========================================================================
+# Site Specific Information (licence-specific questions)
+# Split into one question per page sometimes for better UX with long labels
+# ==========================================================================
+has_page site_pedestrian_space => (
+    fields => ['site_adequate_space', 'footway_incursion', 'continue'],
+    title => 'Pedestrian space',
+    next => 'site_carriageway_distance',
+);
+
+has_field site_adequate_space => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    label => 'Will pedestrian space be maintained in line with TfL requirements?',
+    required => 1,
+    tags => { hint => 'A minimum width of 2m on lightly used footways, 3m on medium‑use footways and 4m on busy footways, with no reduction on intensely used footways.' },
+    options => [
+        { label => 'Yes', value => 'Yes' },
+        { label => 'No', value => 'No' },
+    ],
+);
+
+has_field footway_incursion => (
+    type => 'Text',
+    label => 'What is the proposed footway incursion?',
+    required => 1,
+    tags => {
+        hint => 'For example, ‘1m from building line and 3m unobstructed footway’ or ‘no footway incursion’',
+    },
+);
+
+# ==========================================================================
+has_page site_carriageway_distance => (
+    fields => ['carriageway_incursion', 'site_within_450mm', 'continue'],
+    title => 'Carriageway impact',
+    next => 'site_infrastructure',
+);
+
+has_field carriageway_incursion => (
+    type => 'Text',
+    label => 'What is the proposed carriageway incursion?',
+    required => 1,
+    tags => {
+        hint => 'For example, ‘no carriageway incursion’ or ‘Materials located in loading bay’',
+    },
+);
+
+has_field site_within_450mm => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    label => 'Will any materials be stored within 450mm of the carriageway edge?',
+    required => 1,
+    options => [
+        { label => 'Yes', value => 'Yes' },
+        { label => 'No', value => 'No' },
+    ],
+);
+
+# ==========================================================================
+has_page site_infrastructure => (
+    fields => ['site_obstruct_infrastructure', 'continue'],
+    title => 'Street infrastructure',
+    next => 'have_you_considered',
+);
+
+has_field site_obstruct_infrastructure => (
+    type => 'Select',
+    widget => 'RadioGroup',
+    label => 'Will the materials obstruct or obscure any street furniture, such as traffic signals, crossings, or signs?',
+    tags => { hint => 'Other examples (not limited to) include bus stops, traffic signal controllers, lighting columns, parking bays, and ironwork.' },
+    required => 1,
+    options => [
+        { label => 'Yes', value => 'Yes' },
+        { label => 'No', value => 'No' },
+    ],
+);
+
+# ==========================================================================
+# Have you considered? (TCSR/TTRO + T&Cs)
+# Fields from Fields::TemporaryProhibition role
+# ==========================================================================
+has_page have_you_considered => (
+    fields => [
+        'parking_dispensation',
+        'parking_bay_suspension',
+        'bus_stop_suspension',
+        'bus_lane_suspension',
+        'road_closure_required',
+        'tcsr_website_note',
+        'continue'
+    ],
+    title => 'Additional considerations',
+    intro => 'building-materials/have_you_considered.html',
+    next => 'terms',
+);
+
+has_page terms => (
+    fields => [
+        'terms_accepted',
+        'continue'
+    ],
+    title => 'Terms and conditions confirmation',
+    next => 'uploads',
+);
+
+# ==========================================================================
+# Upload required documents
+# ==========================================================================
+my $upload_fields = [
+    'upload_insurance',
+    'insurance_validity',
+    'upload_rams',
+    'upload_site_drawing',
+    'upload_additional',
+    'continue'
+];
+has_page uploads => (
+    fields => $upload_fields,
+    title => 'Upload required documents',
+    intro => 'uploads.html',
+    next => 'payment',
+    update_field_list => sub {
+        my ($form) = @_;
+        my $fields = {};
+        foreach (@$upload_fields) {
+            next unless $_ =~ /^upload_/;
+            $form->handle_upload($_, $fields);
+        }
+        return $fields;
+    },
+    post_process => sub {
+        my ($form) = @_;
+        foreach (@$upload_fields) {
+            next unless $_ =~ /^upload_/;
+            $form->process_upload($_);
+        }
+    },
+);
+
+has_field upload_site_drawing => (
+    type => 'FileIdUpload',
+    label => 'Site drawing',
+    tags => {
+        hint => 'Including measurements and available space maintained for pedestrians',
+    },
+    messages => {
+        upload_file_not_found => 'Please upload a site drawing',
+    },
+);
+
+has_field upload_additional => (
+    type => 'FileIdUpload',
+    label => 'Additional supporting documentation',
+    messages => {
+        upload_file_not_found => 'Please upload any additional supporting documentation',
+    },
+);
+
+__PACKAGE__->meta->make_immutable;
+
+1;